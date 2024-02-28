f23abf1ca930d679cde6d6b64de7d7b680d2fc03
ashtonpaulsen <ajp86@byu.edu>
2024-02-28 12:03:51 -0700

    updated with all columns grouped by related data and by variable type.

--- analysis.R
+++ analysis.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 # Import Data
 df <- read_csv('https://www.dropbox.com/scl/fi/umsuyuq3c4gj0bju6arfw/11_train.csv?rlkey=ty34yltxl41eycqbccidrgjxz&dl=1')
-df
+df %>% glimpse()
 
 # boxplots of every column in the data set 
 df %>% 
@@ -10,26 +10,77 @@ df %>%
   geom_boxplot() + 
   facet_wrap(~name, scales = 'free_y')
 
-# various groupings
-health_conditions <- c('Diabetes_binary', 'HighBP', 'HighChol', 'Stroke', 'HeartDiseaseorAttack')
-lifestyle_factors <- c('Smoker', 'PhysActivity', 'Fruits')
-health_monitoring
+# ***************various groupings**********************
+# health related variables
+health_cols <- c("BMI", "GenHlth", "MentHlth", "PhysHlth", "DiffWalk", "HighBP", "HighChol", "Stroke", "HeartDiseaseorAttack")
+# Lifestyle grouping
+life_cols <- c("Fruits", "Veggies", "HvyAlcoholConsump", "Smoker", "PhysActivity")
+# Demographic Variables
+demo_cols <- c("Age", "Sex", "Education", "Income")
+# Binary Variables
+binary_cols <- c("Diabetes_binary", "HighBP", "HighChol", "CholCheck", 
+                "Smoker", "Stroke", "HeartDiseaseorAttack", "PhysActivity", 
+                "Fruits", "Veggies", "HvyAlcoholConsump", "AnyHealthcare", 
+                "NoDocbcCost")
 
 # categorical and continuous variables
-categorical_variables <- c('Diabetes_binary','HighBP','HighChol','CholCheck','Smoker','Stroke','HeartDiseaseorAttack','PhysActivity','Fruits')
-continous_variables <- 'BMI'
+categorical_variables <- c(
+  "Diabetes_binary",
+  "HighBP",
+  "HighChol",
+  "CholCheck",
+  "Smoker",
+  "Stroke",
+  "HeartDiseaseorAttack",
+  "PhysActivity",
+  "Fruits",
+  "Veggies",
+  "HvyAlcoholConsump",
+  "AnyHealthcare",
+  "NoDocbcCost",
+  "Sex",
+  "Education",
+  "GenHlth",
+  "DiffWalk",
+  "Income"
+)
+
+continuous_variables <- c(
+  "BMI",
+  "MentHlth",
+  "PhysHlth",
+  "Age"
+)
 
 df %>% 
   select(all_of(categorical_variables))
 
-bmi_cap <- quantile(df$BMI, .99) 
+# distribution of all continuous variables
+continuous_distribution_plot <- df %>%
+  select(all_of(continuous_variables)) %>%
+  pivot_longer(cols = everything(), names_to = "variable", values_to = "value") %>%
+  ggplot(aes(x = value)) +
+  geom_histogram(fill = "blue", bins = 20) +
+  facet_wrap(~ variable, scales = "free") +
+  labs(x = "Value", y = "Frequency", title = "Histograms of Continuous Variables")
 
-# distribution of BMI values
-df %>% 
-  select(BMI, Diabetes_binary) %>% 
-  ggplot(aes(x = BMI)) + 
-  geom_histogram(fill = "blue")
-  
+# Select categorical columns and convert to long format
+df_categorized <- df %>%
+  select(all_of(categorical_variables)) %>%
+  pivot_longer(cols = everything(), 
+               names_to = "variable", 
+               values_to = "value")
+
+# distribution of all categorical variables
+categorical_disribution_plot <- ggplot(df_categorized, aes(x = value, fill = variable)) +
+  geom_bar() +
+  facet_wrap(~variable, scales = "free") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = "Category", y = "Count", fill = "Variable",
+       title = "Distribution of Categorical Variables")
+
+# count of missing data in each column
+missing_counts <- colSums(is.na(df))
 
 df %>% 
-  glimpse()
+  count(DiffWalk)
